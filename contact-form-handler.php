1411958034f887e05a104ac25c88d223c1b7405f
Soham Deshmukh <82657653+sohamdd0109@users.noreply.github.com>
2021-11-07 12:28:38 +0530

    added-contact_form-and-more

---
+++ contact-form-handler.php
@@ -0,0 +1,30 @@
+<?php
+ $name = $_POST ['name'];
+ $visitor_email = $_POST ['email'];
+ $message = $_POST ['message';]
+
+ $email_from = 'robotinsane01@gmail.com';
+
+ $email_subject = "New Form Submission from https://sohamdd0109.github.io";
+
+ $email_body = "Name: $name.\n".
+                    "Email: $visitor_email.\n".
+                        "Message: $message.\n";
+
+
+ $to = 'robotinsane01@gmail.com';
+
+ $headers = "From: $email_from \r\n";
+ $headers .="Reply-to: $visitor_email \r\n";
+
+ $mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact-me.html");
+
+
+
+
+
+
+
+?>
